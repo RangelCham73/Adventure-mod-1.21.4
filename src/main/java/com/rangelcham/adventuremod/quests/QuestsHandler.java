67fba5bd8ce816e3a17fd4ee5736804644ac51bf
Diego <drangelsanz@gmail.com>
2025-05-12 18:14:58 +0200

    rollback de misiones

--- src/main/java/com/rangelcham/adventuremod/quests/QuestsHandler.java
+++ src/main/java/com/rangelcham/adventuremod/quests/QuestsHandler.java
@@ -2,20 +2,17 @@ package com.rangelcham.adventuremod.quests;
 
 import com.rangelcham.adventuremod.AdventureMod;
 import com.rangelcham.adventuremod.nbt.PlayerQuestHandler;
-import com.rangelcham.adventuremod.quests.data.ExcelReader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.commands.CommandSourceStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
-import org.apache.poi.ss.usermodel.Workbook;
 import java.util.*;
 
 @EventBusSubscriber(modid = AdventureMod.MODID)
 public class QuestsHandler {
 
-    private static ExcelReader excelReader = new ExcelReader();
     public static HashMap<Integer, Quest> quests = new HashMap<>();
 
     @SubscribeEvent
@@ -26,15 +23,14 @@ public class QuestsHandler {
             Minecraft mc = Minecraft.getInstance();
             if (mc.screen == null) {
                 QuestScreen screen = new QuestScreen();
-                screen.setMissions(excelReader.readExcel().values());
+                screen.setMissions(quests.values());
                 mc.setScreen(screen);
             }
         }
     }
 
     public static void doStep(CommandSourceStack command, int id, int step) {
-        Quest quest = excelReader.readExcel().get(id);
-        quests.put(id, quest); // Guardar en memoria para luego hacer el guardado de datos en el fichero NBT
+        Quest quest = quests.get(id);
         if (quest != null) {
             if (quest.isActive && step > 0) {
                 quest.currentStep = step;
